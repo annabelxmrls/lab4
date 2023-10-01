d1ff31242a1b5f176aaef9338cac287d57fc9618
Annabel Morales <annabelxmrls@gmail.com>
2023-09-30 22:03:13 -0700

    fixing errors

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', init)
 function init() {
 
   const prev_btn = 
-document.querySelector(".prev.btn");
+document.querySelector(".prev-btn");
   const next_btn = 
 document.querySelector(".next-btn");
   const frame = 
