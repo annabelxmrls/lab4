d2789f5f8b98c03b29692806d36b7019419e2af5
Annabel Morales <annabelxmrls@gmail.com>
2023-09-30 21:42:23 -0700

    shortcuts

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', init)
+
+function init() {
+
+  const prev_btn = 
+document.querySelector(".prev.btn");
+  const next_btn = 
+document.querySelector(".next-btn");
+  const frame = 
+document.querySelector(".frame");
+  const slides = frame.querySelectorAll("img");
+}
+
